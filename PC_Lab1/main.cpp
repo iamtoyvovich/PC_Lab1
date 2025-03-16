ae1bfe0bbe746350a23e71fa56ba2ff7d14619b7
Yevhen Zaiets <zaiets.yevhen@lll.kpi.ua>
2025-03-16 09:57:05 +0200

    First commit with existing project files

---
+++ PC_Lab1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PC_Lab1
+//
+//  Created by Yevhen Zaiets on 3/16/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
