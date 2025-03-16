8d7b6559607d2407e12731b27a0fa3622f53324c
Yevhen Zaiets <zaiets.yevhen@lll.kpi.ua>
2025-03-16 15:00:00 +0200

    додав дані про процесор Apple M2 Pro, щоб мати розуміння як далі проводити роботу

--- PC_Lab1/main.cpp
+++ PC_Lab1/main.cpp
@@ -3,10 +3,34 @@
 #include <cstdlib>
 #include <ctime>
 #include <chrono>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <thread>
+
+void printSystemInfo() {
+    unsigned int logicalCores = std::thread::hardware_concurrency();
+    std::cout << "Logical cores: " << logicalCores << "\n";
+    
+    int mib[2], physicalCores = 0;
+        size_t len = sizeof(physicalCores);
+        mib[0] = CTL_HW;
+        mib[1] = HW_AVAILCPU;
+        sysctl(mib, 2, &physicalCores, &len, nullptr, 0);
+        std::cout << "Physical cores: " << physicalCores << "\n";
+
+        int64_t memSize = 0;
+        size_t sizeLen = sizeof(memSize);
+        int mibMem[2] = {CTL_HW, HW_MEMSIZE};
+        sysctl(mibMem, 2, &memSize, &sizeLen, nullptr, 0);
+        std::cout << "RAM approx: " << (memSize >> 20) << " MB\n";
+}
 
 
 int main() {
-    int N = 1000;
+    
+    printSystemInfo();
+    
+    int N = 10;
     std::srand(static_cast<unsigned>(std::time(nullptr)));
 
     
@@ -16,6 +40,15 @@ int main() {
                 matrix[i][j] = std::rand() % 100;
             }
         }
+    
+    /*std::cout << "Початкова матриця:\n";
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                std::cout << matrix[i][j] << "\t";
+            }
+            std::cout << "\n";
+        }
+        std::cout << "\n";*/
     
     auto start = std::chrono::high_resolution_clock::now();
     
